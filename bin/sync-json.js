e4bb5cadd16617d00cc18b7c36e1318de81e4680
fengdachao <fengdachao@aliyun.com>
2018-01-11 19:32:38 +0800

    Sync json file command.

---
+++ bin/sync-json.js
@@ -0,0 +1,48 @@
+var colorize, diff, fs, tty;
+
+fs = require('fs');
+
+tty = require('tty');
+
+diff = require('../lib/index').diff;
+
+colorize = require('../lib/colorize').colorize;
+
+search = require('../lib/creator').search;
+generate = require('../lib/creator').generate;
+
+(function(argv) {
+    var data1, data2, json1, json2, options, result;
+    options = require('dreamopt')(["Usage: json-diff [-vjCk] first.json second.json", "Arguments:", "  first.json              Old file #var(file1) #required", "  second.json             New file #var(file2) #required", "General options:", "  -v, --verbose           Output progress info", "  -C, --[no-]color        Colored output", "  -j, --raw-json          Display raw JSON encoding of the diff #var(raw)", "  -k, --keys-only         Compare only the keys, ignore the differences in values #var(keysOnly)"], argv);
+    if (options.verbose) {
+      process.stderr.write((JSON.stringify(options, null, 2)) + "\n");
+    }
+    if (options.verbose) {
+      process.stderr.write("Loading files...\n");
+    }
+    data1 = fs.readFileSync(options.file1, 'utf8');
+    data2 = fs.readFileSync(options.file2, 'utf8');
+    if (options.verbose) {
+      process.stderr.write("Parsing old file...\n");
+    }
+    json1 = JSON.parse(data1);
+    process.stderr.write("Parsing new file...\n");
+    json2 = JSON.parse(data2);
+    process.stderr.write("Running diff...\n");
+    result = diff(json1, json2, options);
+    if (options.color == null) {
+      options.color = tty.isatty(process.stdout.fd);
+    }
+    if (result) {
+      var creatorResult = search(result);
+      process.stderr.write("Generate sync json file...\n");
+      generate(json2, creatorResult.missing, creatorResult.extra);      
+    } else {
+      if (options.verbose) {
+        process.stderr.write("No diff");
+      }
+    }
+    if (result) {
+      return process.exit(1);
+    }
+  }).apply(this, process.argv.slice(2));
