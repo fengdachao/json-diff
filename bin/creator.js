141805c284dacdce6b132f2b6b7712fc558a3629
fengdachao <fengdachao@aliyun.com>
2018-01-23 19:41:51 +0800

    Fix a bug when missing a object not only a key.

--- bin/creator.js
+++ bin/creator.js
@@ -1,20 +1,57 @@
 (function() {
+	var TAB = '\t', ENTER = '\r\n';
 	var fs = require('fs');
 
 	var missing = [], extra = [];
 
+	function isArray(obj) {
+		return Object.prototype.toString.call(obj) === '[object Array]';
+	}
+
+	function isObject(obj) {
+		return Object.prototype.toString.call(obj) === '[object Object]';
+	}
+
+	function getObjectKeys(obj, pkey) {
+		var result = [];
+		for(var key in obj) {
+			var val = obj[key];
+			if (typeof val === 'string' || isArray(val)) {
+				result.push(key);
+			} else if(isObject(val)) {
+				result = result.concat(getObjectKeys(val, pkey + key + '.'));
+			}
+		}
+		return result;
+	}
+	
 	function searchKey(data, pkey) {
 		for(var key in data) {
-			var val = data[key];
-			if (typeof val === 'string') {
-				if (key.indexOf('__deleted') !== -1) {
-					missing.push(pkey + key.slice(0, -9))
-				} else if (key.indexOf('__added') !== -1) {
-					extra.push(pkey + key.slice(0, -7))
+			var val = data[key],
+				optionKeys = [];
+
+			if (key.indexOf('__deleted') !== -1 || key.indexOf('__added') !== -1) {
+				if (typeof val === 'string') {
+						optionKeys.push(pkey + (key.indexOf('__deleted') !== -1 ? key.slice(0, -9) : key.slice(0, -7)));
+				} else if (isObject(val)) {
+					optionKeys = optionKeys.concat(getObjectKeys(val, pkey + key + '.'));
 				}
-			} else if (typeof val === 'object') {
-				searchKey(val, pkey + key + '.')
+				key.indexOf('__deleted') !== -1 ?
+					missing = missing.concat(optionKeys) :
+					extra = extra.concat(optionKeys);
+			} else if (isObject(val)) {
+				searchKey(val, pkey + key + '.');
 			}
+
+			// if (typeof val === 'string') {
+			// 	if (key.indexOf('__deleted') !== -1) {
+			// 		missing.push(pkey + key.slice(0, -9))
+			// 	} else if (key.indexOf('__added') !== -1) {
+			// 		extra.push(pkey + key.slice(0, -7))
+			// 	}
+			// } else if (typeof val === 'object') {
+			// 	searchKey(val, pkey + key + '.')
+			// }
 		}
 	}
 
@@ -27,6 +64,7 @@
 			key = keys[i];
 			_data = _data[key];
 		}
+		key = keys[i];
 		if (opt === 'add') {
 			_data[key] = value;
 		} else {
@@ -54,6 +92,43 @@
 		return result;
 	}
 
+	function formatter(jsonData, indent) {
+		var val = '', result = '';
+		if (isObject(jsonData)) {
+			result += '{' + ENTER;
+			for(var key in jsonData) {
+				val = jsonData[key];
+				if (val === undefined) continue;
+				result += indent + TAB + '"' + key + '" : ';
+				if ( typeof val !== 'object' ) {
+					val = val.replace(/"/g, '\'');
+					result += '"' + val + '",' + ENTER;
+				} else {
+					result += formatter(val, indent + TAB + TAB);
+				}
+			}
+			result = result.slice(0, -3) + ENTER;
+			result += indent + '},' + ENTER;
+		} else if (isArray(jsonData)) {
+			result += '[' + ENTER;
+			for(var index = 0; index < jsonData.length; index++) {
+				val = jsonData[index];
+				if ( typeof val !== 'object' ) {
+					val = val.replace(/"/g, '\'');
+					result += indent + TAB + '"' + val + '"';
+					if (index === jsonData.length - 1) result += ENTER;
+					else result += ',' + ENTER;
+				} else {
+					result += formatter(val, indent + TAB);
+				}
+			}
+			result += indent + '],' + ENTER;
+		} else {
+			result += indent + jsonData;
+		}
+		return result;
+	}
+
 	module.exports = {
 		search: function(result) {
 			searchKey(result, '')
@@ -76,9 +151,11 @@
 			}
 			var fileRegExp = /[a-zA-Z]+\.[a-z]+/;
 			var newFileName = fileRegExp.exec(fileName)[0]
-			fs.writeFileSync('../result/' + newFileName.replace(/\.[a-z]+/, '.json'), JSON.stringify(data));
-			fs.writeFileSync('../result/' + newFileName.replace(/\.[a-z]+/, '-append.json'), JSON.stringify(missing))
-			fs.writeFileSync('../result/' + newFileName.replace(/\.[a-z]+/, '-remove.json'), JSON.stringify(extra))
+			var formatterJson = JSON.stringify(data, null, 2); //formatter = formatter(data, TAB, formatterJson);
+
+			fs.writeFileSync('../result/' + newFileName.replace(/\.[a-z]+/, '.json'), formatterJson);
+			fs.writeFileSync('../result/' + newFileName.replace(/\.[a-z]+/, '-append.json'), JSON.stringify(missing, null, 2))
+			fs.writeFileSync('../result/' + newFileName.replace(/\.[a-z]+/, '-remove.json'), JSON.stringify(extra, null, 2))
 		}
 	}
 }).call(this)
