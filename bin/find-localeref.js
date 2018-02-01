78bc4f0326d90e10729af29d4d2ee17d371db6d8
fengdachao <fengdachao@aliyun.com>
2018-02-01 16:17:51 +0800

    Diff fInd reference and json file you provide.

--- bin/find-localeref.js
+++ bin/find-localeref.js
@@ -18,7 +18,7 @@ search = require('../lib/creator').search;
   localeContent = fs.readFileSync(options.jsonFile);
   localeContent = JSON.parse(localeContent);
   refKeys = findRef(options.dirPath, true);
-  result = diff(localeContent, refKeys, {keysOnly: true});
+  result = diff(refKeys, localeContent, {keysOnly: true});
   creatorResult = search(result);
   generate('ref-info.js', JSON.parse(JSON.stringify(refKeys)), creatorResult.missing, creatorResult.extra, result);
   fs.writeFileSync('result/ref.json', JSON.stringify(refKeys, null, 2));
